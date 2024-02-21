b96d21263c6fbb0bb4aaa40c282058925a33d82d
DaanHollands <daan.hollands@student.uhasselt.be>
2024-02-21 20:14:58 +0100

    Finished CheckNeighboursInGrid Class + Test in main class

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Integer> speelVeld= new ArrayList<>();
+        speelVeld.add(0);speelVeld.add(0);speelVeld.add(1);speelVeld.add(0);speelVeld.add(1);
+        speelVeld.add(1);speelVeld.add(2);speelVeld.add(1);speelVeld.add(1);speelVeld.add(0);
+        speelVeld.add(0);speelVeld.add(2);speelVeld.add(1);speelVeld.add(2);speelVeld.add(0);
+        speelVeld.add(1);speelVeld.add(1);speelVeld.add(0);speelVeld.add(1);speelVeld.add(1);
+        speelVeld.add(2);speelVeld.add(1);speelVeld.add(0);speelVeld.add(0);speelVeld.add(0);
+
+        System.out.println(CheckNeighboursInGrid.getSameNeighboursIds(speelVeld, 5, 5, 11));
 
     }
 }
